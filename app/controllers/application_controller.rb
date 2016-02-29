f46123d37f7f1e4b55c6db8a34b0f3d66de843e5
Marcelo Boeira <me@marceloboeira.com>
2016-02-29 20:43:40 -0300

    feature: Rescue from mongo errors with a not_found default action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from Mongoid::Errors::DocumentNotFound, with: :not_found
+
+  def not_found
+    redirect_to root_path, status: :not_found
+  end
 end
